d9582fe0c7a32e9df8f0b9510ade4b9b671c1541
agbakwuruchika <agbakwuruchika3@gmail.com>
2024-05-13 13:55:43 -0700

    nineteenth commit

---
+++ src/app/(schools-in-nigeria-folder)/(universities-in-nigeria-folder)/(federal-universities-in-nigeria-folder)/(modibbo-adama-university-mautech-folder)/mautech-courses/page.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import TopAppBar from '@/components/top-app-bar'
+import NavigationDrawer from '@/components/navigation-drawer'
+import SideSheets from '@/components/side-sheets'
+import SocialMediaPostCard from '@/components/social-media-post-card'
+import BottomAppBar from '@/components/bottom-app-bar'
+import { Metadata } from 'next'
+
+
+export const metadata:Metadata = {
+  title: "Abubakar Tafawa Balewa University (ATBU)"
+}
+
+
+export default function ATBU() {
+  return (
+    <div className="overAllContainer flex min-h-screen flex-col relative"> 
+      <TopAppBar title = "Home Feed"/>
+      <div className = "body-container flex px-4 mt-4 relative">
+        <div className = "left-side-bar w-96 fixed hidden md:block"> 
+          <NavigationDrawer />  
+        </div>
+        <div className = "main-content md:ml-96 md:mr-4 w-full md:flex-grow">
+          
+          <h1>Modibbo Adama University of Technology (MAUTECH) Courses</h1>
+        </div>
+        <div className = "right-side-bar w-96 hidden md:block">
+          <SideSheets />
+
+        </div>
+          
+      </div>
+      <BottomAppBar />
+      
+    </div>
+  )
+}
